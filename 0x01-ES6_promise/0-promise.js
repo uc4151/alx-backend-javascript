328b3688829f09755e2253d3c6571d72cc87ba0a
uc4151 <gemmalinda19@gmail.com>
2024-08-09 10:03:54 +0000

    setup redo

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,3 @@
 export default function getResponseFromAPI() {
-//exporting the function so it can be imported
-    return new Promise((resolve, reject) => { 
-        // resolving the promise
-        resolve();
-    });
+  return new Promise(() => {});
 }
